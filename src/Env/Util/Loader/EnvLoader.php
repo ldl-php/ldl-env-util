6115ec33c9cd5a9d526f1cee068e27ca250b2b2b
Federico Stange <jpfstange@gmail.com>
2022-01-21 11:25:35 +0000

    feature/1201698987280922 - Add EnvLoader::loadFile method

--- src/Env/Util/Loader/EnvLoader.php
+++ src/Env/Util/Loader/EnvLoader.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace LDL\Env\Util\Loader;
 
+use LDL\Env\Util\File\Parser\EnvFileParser;
 use LDL\Env\Util\Line\Collection\EnvLineCollectionInterface;
 use LDL\Env\Util\Line\Type\Variable\EnvLineVarInterface;
 use LDL\Framework\Helper\IterableHelper;
@@ -20,5 +21,18 @@ final class EnvLoader
             putenv((string) $l);
             $_ENV[$l->getVar()] = $l->getValue();
         });
+    }
+
+    /**
+     * This method must be used with an .env file which has already been compiled.
+     *
+     * @throws \LDL\Env\Util\File\Exception\ReadEnvFileException
+     */
+    public static function loadFile(string $file): void
+    {
+        self::load(
+            (new EnvFileParser(null, false))
+                ->parse([$file], false)
+        );
     }
 }
